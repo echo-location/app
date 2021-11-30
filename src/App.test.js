a79c934f6214cd0ea9c4f76b9050b237591cdb9b
Daniel Kao <78634083+Dankco@users.noreply.github.com>
2021-11-30 00:12:00 -0800

    test hotfix

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+window.URL.createObjectURL = function() {};
 test('renders items link', () => {
   render(<App />);
   const linkElement = screen.getByText(/items/i);
