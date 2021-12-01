c8823872d8c786248941aea179f081fa1c8063a2
Matthew Yu <56417255+matthewyu01@users.noreply.github.com>
2021-11-30 19:47:00 -0800

    Centered map and set correct zoom level

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,8 +1,43 @@
 import React from "react";
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import ReactMapboxGl, { Layer, Feature, Popup } from "react-mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { useEffect, useState } from "react";
+import { getItems } from "../utils/utils";
 
 const Map = () => {
+  const [coordinates, setCoordinates] = useState([]);
+  const center = [-118.4453, 34.071];
+
+  useEffect(() => {
+    const fetchCoordinates = async () => {
+      const items = await getItems();
+
+      const coords = items.items
+        .map((item) => {
+          console.log(item);
+          const c = item.meta.coordinates;
+          const itemName = item.name;
+          if (c.length == 0) return [];
+          const [lat, long] = c;
+          return [long, lat];
+        })
+        .filter((item) => item.length > 0);
+
+      let newCenter = [0.0, 0.0];
+      coords.map((coord) => {
+        if (coord.length === 2) {
+          console.log(coord);
+          const [lat, long] = coord;
+          newCenter[0] += lat;
+          newCenter[1] += long;
+        }
+      });
+
+      setCoordinates(coords);
+    };
+    fetchCoordinates();
+  }, []);
+
   const Map = ReactMapboxGl({
     accessToken:
       "pk.eyJ1Ijoic3dlZW5leW5nbyIsImEiOiJja3dsb25jbzQyM3I1MnBwd2RtemFldWhpIn0.VTSZL-ecZvL5SK7S1uL9iw",
@@ -11,15 +46,34 @@ const Map = () => {
   return (
     <div style={{ background: "red" }}>
       {/* eslint-disable-next-line */}
-      <Map style= "mapbox://styles/mapbox/streets-v9"
+      <Map
+        style="mapbox://styles/mapbox/streets-v9"
         containerStyle={{
           height: "37rem",
           width: "100%",
         }}
+        center={center}
+        zoom={[14.5]}
       >
-        <Layer type="symbol" id="marker" layout={{ "icon-image": "marker-15" }}>
-          <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-        </Layer>
+        {coordinates.length > 0 && (
+          <>
+            <Layer
+              type="symbol"
+              id="marker"
+              layout={{ "icon-image": "harbor-15" }}
+            >
+              {coordinates.map((coord) => (
+                <Feature coordinates={coord} />
+              ))}
+            </Layer>
+
+            {coordinates.map((coord) => (
+              <Popup coordinates={coord}>
+                <h1>Popup</h1>
+              </Popup>
+            ))}
+          </>
+        )}
       </Map>
     </div>
   );
