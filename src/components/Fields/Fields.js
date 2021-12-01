daeb40992f02f1e862cd862934b7be489371ac09
Sweeney Ngo <sweeneyngo@gmail.com>
2021-11-30 19:05:15 -0800

    Refactored fields component for validation

---
+++ src/components/Fields/Fields.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import "./Fields.css";
+
+const Fields = ({ user, updateLogin }) => {
+  useEffect(() => {
+    console.log(user);
+  }, []);
+  return (
+    <Box
+      component="form"
+      sx={{
+        "& .MuiTextField-root": { m: 1, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div className="fields">
+        {user.map(({ name, displayName, type, help, error }) => (
+          <TextField
+            required
+            key={name}
+            type={type}
+            id={name}
+            label={displayName}
+            error={error}
+            helperText={error ? help : ""}
+            onChange={(e) => updateLogin(name, e.target.value)}
+          />
+        ))}
+      </div>
+    </Box>
+  );
+};
+
+export default Fields;
